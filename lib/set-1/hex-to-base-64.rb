016fb7b1efef48fdfe0136c3f18a6f7153b8169b
Sean Smith <seansmith1020@gmail.com>
2014-08-12 08:41:47 -0600

    set-1 problem-1 complete

---
+++ lib/set-1/hex-to-base-64.rb
@@ -0,0 +1,12 @@
+require 'base64'
+
+# http://cryptopals.com/sets/1/challenges/1/
+
+string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+def hex_to_base64(string)
+  [[string].pack("H*")].pack("m0")
+end
+
+p hex_to_base64(string)
+
