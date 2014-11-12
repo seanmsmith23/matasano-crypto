86e5ca69b42373945018718f2ea6a3f98c5b5f4d
Sean Smith <seansmith1020@gmail.com>
2014-11-12 11:41:00 -0700

    working aes ecb decryption

--- lib/set-1/07-AES.rb
+++ lib/set-1/07-AES.rb
@@ -2,15 +2,53 @@ require "OpenSSL"
 require "Base64"
 require 'awesome_print'
 
-filepath = "/Users/seansmith/gSchoolWork/side-projects/matasano-crypto/lib/set-1/07-file"
+class AESinECB
 
-opened = File.open(filepath)
-read = File.read(opened)
+  def initialize
+    @cipher = OpenSSL::Cipher.new('AES-128-ECB')
+  end
 
-base = Base64.decode64(read)
+  def decrypt(key, filepath, options={})
+    raw = get_raw_content(filepath, options)
+    decrypted = @cipher.decrypt
+    decrypted.key = key
+    decrypted.update(raw) + decrypted.final
+  end
 
-cipher = OpenSSL::Cipher.new('AES-128-ECB')
+  private
 
-decrypted = cipher.decrypt
-decrypted.key = "YELLOW SUBMARINE"
-ap decrypted.update(base) + decrypted.final
+  def get_raw_content(filepath, options)
+    default = { encoding: :base_64 }
+    contents = read_file(filepath)
+    remove_encoding(contents, default.merge(options))
+  end
+
+  def read_file(filepath)
+    opened = File.open(filepath)
+    File.read(opened)
+  end
+
+  def remove_encoding(contents, options)
+    encoding = options.fetch(:encoding)
+    case encoding
+      when :base_64
+        base_64(contents)
+      else
+        raise "No encoding parameter provided"
+    end
+  end
+
+  def base_64(contents)
+    Base64.decode64(contents)
+  end
+
+end
+
+# filepath = "/Users/seansmith/gSchoolWork/side-projects/matasano-crypto/lib/set-1/07-file"
+#
+# base =
+# cipher = OpenSSL::Cipher.new('AES-128-ECB')
+#
+# decrypted = cipher.decrypt
+# decrypted.key = "YELLOW SUBMARINE"
+# ap decrypted.update(base) + decrypted.final
